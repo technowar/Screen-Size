5742fb3f16f34a6bb434df2333e9ef821afbbf83
Johmel Rey Pintor <jahmgwapo@skolpad.org>
2013-05-29 18:10:39 +0800

    Initial commit for the project

---
+++ Screen Size/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Screen Size
+//
+//  Created by OSX on 5/29/13.
+//  Copyright (c) 2013 OSX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
